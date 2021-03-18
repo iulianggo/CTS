17ad30993b811bc57e633f153c36a6617230f43c
iulian_gogoasa <iulianggo@gmail.com>
2021-03-18 07:53:46 +0200

    added eager initialization singleton

---
+++ Seminar4/src/ro/ase/cts/clase/ClinicaVeterinaraEager.java
@@ -0,0 +1,53 @@
+package ro.ase.cts.clase;
+
+public class ClinicaVeterinaraEager {
+	private String nume;
+	private String adresa;
+	private int nrMedici;
+	private float buget;
+	//Instanta statica este initializata la momentul declararii
+	private static final ClinicaVeterinaraEager instanta = new ClinicaVeterinaraEager();
+	
+	private ClinicaVeterinaraEager() {
+		this.nume = "ClinVet";
+		this.adresa = "Str. Clinica";
+		this.nrMedici = 4;
+		this.buget = 1000;
+	}
+	
+	public static ClinicaVeterinaraEager getInstanta() {
+		return instanta;
+	}
+
+	public String getNume() {
+		return nume;
+	}
+
+	public void setNume(String nume) {
+		this.nume = nume;
+	}
+
+	public String getAdresa() {
+		return adresa;
+	}
+
+	public void setAdresa(String adresa) {
+		this.adresa = adresa;
+	}
+
+	public int getNrMedici() {
+		return nrMedici;
+	}
+
+	public void setNrMedici(int nrMedici) {
+		this.nrMedici = nrMedici;
+	}
+
+	public float getBuget() {
+		return buget;
+	}
+
+	public void setBuget(float buget) {
+		this.buget = buget;
+	}
+}
